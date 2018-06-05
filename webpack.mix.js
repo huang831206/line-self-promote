acda90303bb3a6eb115d479e4dd26f809589d843
ben <huang831206@gmail.com>
2018-06-05 12:13:55 +0800

    configure webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,32 @@ let mix = require('laravel-mix');
  |
  */
 
+ mix.webpackConfig({
+ 	resolve: {
+ 		alias: {
+ 			jquery: "jquery/src/jquery"
+ 		}
+ 	}
+ });
+
+// TODO: extract used semantic js files only
 mix.js('resources/assets/js/app.js', 'public/js')
+   .js('resources/assets/js/index.js', 'public/js')
+   .js([
+       'resources/assets/js/utils/EventBus.js',
+       'resources/assets/js/utils/Information.js',
+       'resources/assets/js/utils/Utils.js',
+    ], 'public/js/utils.js')
    .extract([
        'axios',
        'vue',
+       'jquery',
        './resources/assets/semantic/dist/semantic'
    ])
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .sass('resources/assets/sass/index.scss', 'public/css');
+
+
+if (Mix.inProduction) {
+    mix.version();
+}
